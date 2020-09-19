391650499db59dd7f4e69a62800083b805fadcf7
“Neeru <neeram85@gmail.com>
2020-09-19 10:48:43 -0600

    Called the Mapquest Facade to get coordinates

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::ForecastController < ApplicationController
   def index
+    coords = MapquestFacade.new.get_coords(location_param)
+    binding.pry
 
+  end
+
+  private
+
+  def location_param
+    params.permit(:location)
   end
 end

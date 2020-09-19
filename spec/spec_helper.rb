c79a1378a2fd8f4546a95c49164474e211180f54
“Neeru <neeram85@gmail.com>
2020-09-19 08:13:55 -0600

    Add vcr configuration to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require 'webmock/rspec'
   require 'simplecov'
   SimpleCov.start
   # rspec-expectations config goes here. You can use an alternate

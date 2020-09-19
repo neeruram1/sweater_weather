699780f3a88769fe4f800728886a069df696dbc5
“Neeru <neeram85@gmail.com>
2020-09-19 09:56:28 -0600

    Added test for weather service, can use coords to return forecast

--- spec/requests/api/v1/services/weather_service_spec.rb
+++ spec/requests/api/v1/services/weather_service_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 describe "Can use coordinates to return forecast" do
   it "Can use coordinates to pull data back from weather API" do
-    
+    location = 'denver, co'
+    latitude = 39.738453
+    longitude = -104.984853
+
+    mapquest_service = MapquestService.new
+    weather_service = WeatherService.new
+
+    response = weather_service.forecast(coords)
+
+    expect(response).to be_a Hash
+    expect(response).to have_key :lat
+    expect(response).to have_key :lon
+    expect(response).to have_key :current
+    expect(response).to have_key :weather
+
+    expect(response[:lat]).to eq(latitude)
+    expect(response[:lat]).to eq(longitude)
   end
 end

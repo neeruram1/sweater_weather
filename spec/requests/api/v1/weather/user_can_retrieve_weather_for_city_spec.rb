d0475c8ffd84caa678098af8f41cbd516d2ea01c
“Neeru <neeram85@gmail.com>
2020-09-19 11:42:55 -0600

    Write test to check current_forecast attributes

--- spec/requests/api/v1/weather/user_can_retrieve_weather_for_city_spec.rb
+++ spec/requests/api/v1/weather/user_can_retrieve_weather_for_city_spec.rb
@@ -10,8 +10,35 @@ describe "User can request forecast for a city" do
 
     weather = JSON.parse(response.body, symbolize_names: true)
 
+    expect(weather).to be_a Hash
     expect(weather[:data][:weather][:attributes]).to have_key :current_forecast
     expect(weather[:data][:weather][:attributes]).to have_key :hourly_forecast
     expect(weather[:data][:weather][:attributes]).to have_key :daily_forecast
+  end
+
+  it "Current forecast returns specific attributes" do
+    location = 'denver, co'
+
+    get "/api/v1/forecast?location=#{location}"
+
+    expect(response).to be_successful
+    response.content_type == "application/json"
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    current_forecast = data[:data][:weather][:attributes][:current_forecast]
+
+    expect(current_forecast[:attributes]).to have_key :current_temp
+    expect(current_forecast[:attributes]).to have_key :current_high
+    expect(current_forecast[:attributes]).to have_key :current_low
+    expect(current_forecast[:attributes]).to have_key :location_name
+    expect(current_forecast[:attributes]).to have_key :date
+    expect(current_forecast[:attributes]).to have_key :time
+    expect(current_forecast[:attributes]).to have_key :description
+    expect(current_forecast[:attributes]).to have_key :sunrise_time
+    expect(current_forecast[:attributes]).to have_key :sunset_time
+    expect(current_forecast[:attributes]).to have_key :feels_like
+    expect(current_forecast[:attributes]).to have_key :humidity
+    expect(current_forecast[:attributes]).to have_key :visibility
+    expect(current_forecast[:attributes]).to have_key :uv_index
   end
 end

2cdb83586db445c9ad7fc47b02226ef9f8d563db
“Neeru <neeram85@gmail.com>
2020-09-19 09:18:56 -0600

    Create get_coords method in MapquestService class

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,19 @@
+class MapquestService
+
+  def get_coords(location)
+    to_json("/location=#{location}")
+  end
+
+  private
+
+  def to_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('http://www.mapquestapi.com/geocoding/v1/address') do |f|
+      f.params[:key] = "#{ENV['mapquest_key']}"
+    end
+  end
+end

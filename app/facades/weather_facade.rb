76bd0cf8a035a2a2283d77cd5d74fa7f56644865
“Neeru <neeram85@gmail.com>
2020-09-20 13:42:36 -0600

    Fixed formatting

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,12 +1,15 @@
 class WeatherFacade
-  def forecast(location_param)
-    weather_data = weather_service.forecast(coords(location_param))
+  def forecast(location)
+    weather_data = weather_service.forecast(coords(location))
     Weather.new(weather_data)
   end
 
   def coords(location)
     data = mapquest_service.coords(location)
-    { lat: data[:locations][0][:latLng][:lat].round(2), lon: data[:locations][0][:latLng][:lng].round(2) }
+    {
+      lat: data[:locations][0][:latLng][:lat].round(2),
+      lon: data[:locations][0][:latLng][:lng].round(2)
+    }
   end
 
   private

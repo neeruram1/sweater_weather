10bf2e51a896e788788d4cc281853077349bd008
“Neeru <neeram85@gmail.com>
2020-09-20 12:26:07 -0600

    Created a new poro for daily forecast with a method to check precipitation

---
+++ app/POROS/daily_forecast.rb
@@ -0,0 +1,21 @@
+class DailyForecast
+  def initialize(data)
+    @date_time = data[:dt]
+    @description = data[:weather][0][:main]
+    @high_temp = data[:temp][:max]
+    @low_temp = data[:temp][:min]
+    @precipitation = total_precipitation(data)
+  end
+
+  def total_precipitation(data)
+    if data[:rain].nil? && data[:snow].nil?
+      return 0
+    elsif data[:snow].nil? && data[:rain].nil? == false
+      data[:rain]
+    elsif data[:rain].nil? && data[:snow].nil? == false
+      data[:snow]
+    else
+      data[:rain] + data[:snow]
+    end
+  end
+end

7dced67a48cd057874a8c5aa8efa253e6dc54d0c
“Neeru <neeram85@gmail.com>
2020-09-20 13:06:50 -0600

    Small refactor

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    WeatherSerializer.new(WeatherFacade.new.forecast(location_param)).serialized_json
+    serialize_weather(weather(location)).serialized_json
   end
 
   private
 
-  def location_param
+  def location
     params.permit(:location)
+  end
+
+  def serialize_weather(data)
+    WeatherSerializer.new(data)
+  end
+
+  def weather(location)
+    WeatherFacade.new.forecast(location)
   end
 end

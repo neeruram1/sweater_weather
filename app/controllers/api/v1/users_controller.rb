5cad18a376c6156b9b7e29e4ab547c4238af2376
“Neeru <neeram85@gmail.com>
2020-09-21 16:13:02 -0600

    Passed in status as an argument to error serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,15 +4,11 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: serialize_user(user)
     else
-      render json: serialize_error(user).bad_request, status: :bad_request
+      render json: serialize_errors(user, 400), status: :bad_request
     end
   end
 
   private
-
-  def serialize_error(data)
-    ErrorSerializer.new(data)
-  end
 
   def serialize_user(data)
     UserSerializer.new(data)
@@ -20,5 +16,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def serialize_errors(data, status)
+    ErrorSerializer.new(data, status).errors
   end
 end

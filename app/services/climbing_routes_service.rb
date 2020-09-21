16d99c80bb313ea30007cc4cee29497963ef7fb3
“Neeru <neeram85@gmail.com>
2020-09-21 09:27:46 -0600

    Create conn and to_json methods in ClimbingRoutesService class

--- app/services/climbing_routes_service.rb
+++ app/services/climbing_routes_service.rb
@@ -2,4 +2,17 @@ class ClimbingRoutesService
   def nearby_routes(coords)
 
   end
+
+  private
+
+  def conn
+    Faraday.new('https://www.mountainproject.com/data/') do |f|
+      f.params[:key] = ENV['mountain_project_key'].to_s
+    end
+  end
+
+  def to_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end

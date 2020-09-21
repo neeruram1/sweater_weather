b46a5d0783b7cfd5ec4e99fc13bdf47adacce188
“Neeru <neeram85@gmail.com>
2020-09-21 11:40:30 -0600

    Fixed retrieval of distance value

--- app/POROS/climbing_route.rb
+++ app/POROS/climbing_route.rb
@@ -12,13 +12,15 @@ class ClimbingRoute
     @type = data[:type]
     @rating = data[:rating]
     @location = data[:location]
-    @distance_to_route = distance(data, city_coordinates)[:distance]
+    @distance_to_route = distance(data, city_coordinates)
   end
 
   def distance(data, city_coordinates)
     city_coords = "#{city_coordinates[:lat]}" + ',' + "#{city_coordinates[:lon]}"
     route_coords = "#{data[:latitude]}" + ',' + "#{data[:longitude]}"
-    mapquest_service.distance_between_routes(city_coords, route_coords )
+    result = mapquest_service.distance_between_routes(city_coords, route_coords)
+
+    result[:route][:distance]
   end
 
   private

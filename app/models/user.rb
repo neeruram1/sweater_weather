79369ec3258fe33d208b3381dc759d91486cd485
“Neeru <neeram85@gmail.com>
2020-09-20 21:17:24 -0600

    Add method to generate api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates_presence_of :api_key
   validates_presence_of :email
   validates_presence_of :password
-end 
+  has_secure_password
+
+  def generate_api_key
+    update(api_key: SecureRandom.hex)
+  end
+end

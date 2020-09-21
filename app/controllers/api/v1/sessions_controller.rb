b19f8293a010bc8f4f4d8b10e5fad87024ed7534
“Neeru <neeram85@gmail.com>
2020-09-21 17:48:37 -0600

    Completed create method for sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,11 @@
-class Api::V1::SessionsController < ApplicationController
+class Api::V1::SessionsController < Api::V1::BaseController
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    else
+      render json: ErrorSerializer.new(user, 400)
+    end
   end
 end

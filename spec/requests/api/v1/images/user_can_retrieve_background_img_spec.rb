61538689038703ce8cef52fa7a4317b99d17a00d
“Neeru <neeram85@gmail.com>
2020-09-20 20:05:09 -0600

    User can get background image

---
+++ spec/requests/api/v1/images/user_can_retrieve_background_img_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+describe "User can request background image for a location" do
+  it "Sends an appropriate background image for a specific city", :vcr do
+    location = 'los angeles, ca'
+
+    get "/api/v1/background?location=#{location}"
+
+    expect(response).to be_successful
+    response.content_type == "application/json"
+
+    image_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(image_data).to be_a Hash
+    expect(image_data[:data][:type]).to eq("image")
+    expect(image_data[:data][:attributes]).to have_key :author
+    expect(image_data[:data][:attributes]).to have_key :source
+    expect(image_data[:data][:attributes]).to have_key :location
+    expect(image_data[:data][:attributes]).to have_key :image_url
+    expect(image_data[:data][:attributes]).to have_key :source_url
+    expect(image_data[:data][:attributes]).to have_key :author_profile_link
+  end
+end

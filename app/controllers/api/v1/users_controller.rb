658b3517e2c667272591909e7d88f284ed132ca0
“Neeru <neeram85@gmail.com>
2020-09-20 21:17:55 -0600

    Create a create method in User controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    render json: serialize_user(User.create(user_params).generate_api_key)
+  end
 
+  private
+
+  def serialize_user(data)
+    UserSerializer.new(data)
+  end
+
+  def user_params
+    params.permit(:email, :password)
   end
 end

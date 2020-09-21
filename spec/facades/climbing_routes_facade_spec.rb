2ff387a9ad25eb8b568116ebb2bc2783f453ba43
“Neeru <neeram85@gmail.com>
2020-09-21 10:10:05 -0600

    Write test for climbing_routes method in ClimbingRoutesFacade spec

--- spec/facades/climbing_routes_facade_spec.rb
+++ spec/facades/climbing_routes_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe 'Climbing routes facade' do
   describe 'Instance methods' do
-    it ".coords", :vcr do
+    it ".coords()", :vcr do
       facade = ClimbingRoutesFacade.new
       latitude = 39.738453
       longitude = -104.984853
@@ -9,6 +9,14 @@ describe 'Climbing routes facade' do
 
       expect(facade.coords(location)[:lat]).to eq(latitude.round(2))
       expect(facade.coords(location)[:lon]).to eq(longitude.round(2))
+    end
+
+    it ".climbing_routes()", :vcr do
+      facade = ClimbingRoutesFacade.new
+      location = 'denver, co'
+
+      expect(facade.climbing_routes(location)).to be_a Array
+      expect(facade.climbing_routes(location[0])).to be_a ClimbingRoute
     end
   end
 end

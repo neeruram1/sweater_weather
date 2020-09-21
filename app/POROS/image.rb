55bf61d56b1671badc1cd5d255c266075aad745b
“Neeru <neeram85@gmail.com>
2020-09-20 20:04:16 -0600

    Created Image PORO

---
+++ app/POROS/image.rb
@@ -0,0 +1,19 @@
+class Image
+  attr_reader :id,
+              :author,
+              :source,
+              :location,
+              :image_url,
+              :source_url,
+              :author_profile_link
+
+  def initialize(results, location)
+    @id = nil
+    @source = 'Unsplash'
+    @location = location[:location]
+    @author = results[:results][0][:user][:name]
+    @image_url = results[:results][0][:urls][:raw]
+    @author_profile_link = results[:results][0][:user][:links][:html]
+    @source_url = 'https://unsplash.com/?utm_source=sweater_weather&utm_medium=referral'
+  end
+end

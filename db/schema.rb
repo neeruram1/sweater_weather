1df8dd4bd0de314185e7e24bd25d59b8ff7c5bdb
“Neeru <neeram85@gmail.com>
2020-09-20 21:17:39 -0600

    Change name of attribute to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_024550) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

59fd5fd2f432aa91c4c97d4e2c327191ba3a2111
“Neeru <neeram85@gmail.com>
2020-09-20 20:04:35 -0600

    Updated image facade to remove mapquest service

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,24 +1,12 @@
 class ImageFacade
   def background_img(location)
-    image_data = image_service.background_img(coords(location))
+    image_data = image_service.background_img(location)
     Image.new(image_data, location)
-  end
-
-  def coords(location)
-    data = mapquest_service.coords(location)
-    {
-      lat: data[:locations][0][:latLng][:lat].round(2),
-      lon: data[:locations][0][:latLng][:lng].round(2)
-    }
   end
 
   private
 
   def image_service
     ImageService.new
-  end
-
-  def mapquest_service
-    MapquestService.new
   end
 end

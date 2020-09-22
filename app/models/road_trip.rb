f76f28d234631afd4aafb7f6c95b4b7dba5d7086
“Neeru <neeram85@gmail.com>
2020-09-22 07:32:20 -0600

    Validations tests passing

--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -1,2 +1,7 @@
 class RoadTrip < ApplicationRecord
+  validates :api_key, presence: true
+  validates :origin, presence: true
+  validates :destination, presence: true
+  validates :travel_time, presence: true
+  belongs_to :user
 end

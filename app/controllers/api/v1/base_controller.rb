9b320a05a39918436e76a9ea83332ab1e12b8e62
“Neeru <neeram85@gmail.com>
2020-09-22 09:56:22 -0600

    Add serialize errors method to base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::BaseController < ApplicationController
   def location
     params.permit(:location)
   end
+
+  def invalid_credentials(user, status)
+    ErrorSerializer.new(user, status).invalid_credentials
+  end
+
+  def serialize_errors(data, status)
+    ErrorSerializer.new(data, status).errors
+  end
 end

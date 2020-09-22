d8039f993553824eb05a6923c8f01272d58ef205
“Neeru <neeram85@gmail.com>
2020-09-22 08:48:08 -0600

    Write sad path test for road trips

--- spec/requests/api/v1/road_trip/user_can_plan_road_trip_spec.rb
+++ spec/requests/api/v1/road_trip/user_can_plan_road_trip_spec.rb
@@ -32,6 +32,34 @@ describe 'A user can plan a road trip' do
     expect(road_trip[:data][:attributes]).to have_key(:arrival_temperature)
     expect(road_trip[:data][:attributes]).to have_key(:arrival_forecast_description)
   end
+
+  it "User can't plan road trip if invalid api key sent", :vcr do
+    michael = User.create(email: 'michael@bluthco.org', password: 'banana', password_confirmation: 'banana')
+
+    post '/api/v1/sessions', params: body
+
+    body = {
+      origin: 'denver, co',
+      destination: 'boulder, co',
+      api_key: '239874255'
+    }
+
+    post '/api/v1/road_trip', params: body
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    response.content_type == "application/json"
+
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(road_trip[:errors]).to have_key(:status)
+    expect(road_trip[:errors]).to have_key(:title)
+    expect(road_trip[:errors]).to have_key(:detail)
+
+    expect(road_trip[:errors][:status]).to eq('401')
+    expect(road_trip[:errors][:title]).to eq('Unauthorized')
+    expect(road_trip[:errors][:detail]).to eq('Invalid credentials')
+  end
 end
 
 #only logged in users can plan road trips, invalid api key sent, no api key sent

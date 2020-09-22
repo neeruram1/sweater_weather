905aeaf4f5bd8ac49a6eda0665668a19da465c26
“Neeru <neeram85@gmail.com>
2020-09-22 09:03:32 -0600

    Users can't plan a road trip if they send a blank api key

--- spec/requests/api/v1/road_trip/user_can_plan_road_trip_spec.rb
+++ spec/requests/api/v1/road_trip/user_can_plan_road_trip_spec.rb
@@ -60,6 +60,34 @@ describe 'A user can plan a road trip' do
     expect(error[:errors][:title]).to eq('Unauthorized')
     expect(error[:errors][:detail]).to eq('Invalid credentials')
   end
+
+  it "User can't plan road trip if no api key sent", :vcr do
+    michael = User.create(email: 'michael@bluthco.org', password: 'banana', password_confirmation: 'banana')
+
+    post '/api/v1/sessions', params: body
+
+    body = {
+      origin: 'denver, co',
+      destination: 'boulder, co',
+      api_key: ''
+    }
+
+    post '/api/v1/road_trip', params: body
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    response.content_type == "application/json"
+
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error[:errors]).to have_key(:status)
+    expect(error[:errors]).to have_key(:title)
+    expect(error[:errors]).to have_key(:detail)
+
+    expect(error[:errors][:status]).to eq(401)
+    expect(error[:errors][:title]).to eq('Unauthorized')
+    expect(error[:errors][:detail]).to eq('Invalid credentials')
+  end
 end
 
 #only logged in users can plan road trips, invalid api key sent, no api key sent
